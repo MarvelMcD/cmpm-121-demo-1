f9e9d89081757c3fee7e9aef3fdaf8a9775dc8fa
MarvelMcD <MarvelMcD@gmail.com>
2024-10-14 10:04:42 -0700

    name change and test button
    starting test

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "Goober Farmer";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const gooberButton = document.createElement("button");
+gooberButton.innerHTML = "goober";
+app.append(gooberButton);
