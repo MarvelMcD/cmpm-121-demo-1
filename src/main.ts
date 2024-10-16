b60bc33ee4c82c7b0623d8751874ae44aeaecfd0
MarvelMcD <MarvelMcD@gmail.com>
2024-10-15 22:12:43 -0700

    Step 2 counter
    added counter that increments on click

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,14 @@ app.append(header);
 const gooberButton = document.createElement("button");
 gooberButton.innerHTML = "🫠 goober 🫠";
 app.append(gooberButton);
+
+const counterDisplay = document.createElement("h2");
+let counter: number = 0;
+counterDisplay.innerHTML = counter.toString() + " goobers";
+app.append(counterDisplay);
+
+gooberButton.addEventListener("click", function ()
+{
+    counter++;
+    counterDisplay.innerHTML = counter.toString() + " goobers";
+});
